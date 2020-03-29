e2f3a5ea721fe5f855ce612ead1d28e9cf61ddcb
zakaria5729 <zakariahossain143@gmail.com>
2020-03-29 16:42:13 +0600

    criteria api retrictions or

--- src/com/zakaria/hibernate/HibernateMain.java
+++ src/com/zakaria/hibernate/HibernateMain.java
@@ -42,8 +42,10 @@ public class HibernateMain {
 		CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
 		Root<User> root = criteriaQuery.from(User.class);
 		criteriaQuery.where(
-			criteriaBuilder.like(root.get("userName"), "user1%"),
-			criteriaBuilder.between(root.get("userId"), 1, 50)
+			criteriaBuilder.or(
+				criteriaBuilder.between(root.get("userId"), 1, 3),
+				criteriaBuilder.between(root.get("userId"), 8, 10)
+			)
 		);
 		
 		List<User> users = session.createQuery(criteriaQuery).getResultList();
